679aa55b308db10bbb17460cc0c1b0310a7d649b
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-04-08 10:29:59 +0100

    changes to Patient class and pom file

--- src/main/java/atu/jdbc/Patient.java
+++ src/main/java/atu/jdbc/Patient.java
@@ -29,8 +29,8 @@ public class Patient {
         void setPatientInsert(Object PatientInsertSQL){
         try (Connection conn = DriverManager.getConnection("url" "password")) ;
             PatientInsertSQL = "INSERT INTO Patient Given Name Values('" + name + "')";
-
+            close connection = conn.close;
         }
-       // close connection = conn.close;   ******Dont forget to close*********
+        //******Dont forget to close*********
 }
 //  System.out.println("Hello world!");

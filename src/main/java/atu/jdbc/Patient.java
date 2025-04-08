f0a75a3dac85a779963b5f32e39fa1411c112fa4
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-04-08 10:28:07 +0100

    changes to Patient class and pom file

--- src/main/java/atu/jdbc/Patient.java
+++ src/main/java/atu/jdbc/Patient.java
@@ -3,6 +3,7 @@ package atu.jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Patient {
     public static void main(String[] args) throws SQLException {
@@ -14,11 +15,22 @@ public class Patient {
         conn = DriverManager.getConnection(URL, USER, PASSWORD)
 
 
-        try (Connection conn = DriverManager.getConnection("url""password")) ;  //?????
+        try (Connection conn = DriverManager.getConnection("url" "password")) ;  // user missing?????
         System.out.println("Connected to Database");
     }catch(SQLException e){
+        System.out.println("Error Database connection not established");
         e.printStackTrace();
+        }
+
+    public String PatientInsertSQL() {
+        Scanner
+                PatientInsertSQL = "INSERT INTO Patient Given Name Values('" + name + "')";
     }
+        void setPatientInsert(Object PatientInsertSQL){
+        try (Connection conn = DriverManager.getConnection("url" "password")) ;
+            PatientInsertSQL = "INSERT INTO Patient Given Name Values('" + name + "')";
+
+        }
        // close connection = conn.close;   ******Dont forget to close*********
 }
 //  System.out.println("Hello world!");
